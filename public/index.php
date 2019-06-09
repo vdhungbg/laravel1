b41207045f649592bdfacf7b002f2593a7990c95
vdhungbg <vdhungbg@gmail.com>
2019-06-09 23:30:08 +0700

    develop_M

--- public/index.php
+++ public/index.php
@@ -46,6 +46,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | through the kernel, and send the associated response back to
 | the client's browser allowing them to enjoy the creative
 | and wonderful application we have prepared for them.
+| vdhungbg studing
 |
 */
 
