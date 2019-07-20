e4b824c779703c124cdedb1cd19f108f62bbaebe
vdhungbg <vdhungbg@gmail.com>
2019-07-20 16:18:08 +0700

    n

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(LinksTableSeeder::class);
+        // $this->call(LinksTableSeeder::class);
         $this->call(BooksTableSeeder::class);
     }
 }
