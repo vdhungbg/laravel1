3df7d8567bac55bb6c48bf9cf51cce19f6429154
vdhungbg <vdhungbg@gmail.com>
2019-08-04 14:43:02 +0700

    test

--- app/Book.php
+++ app/Book.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    // git@github.com:vdhungbg/laravel1.git
     // protected $fillable = ['book_name', 'isbn_no', 'book_price'];
     protected $fillable = ['book_name', 'isbn_no', 'book_price'];
 }
