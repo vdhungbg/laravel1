33c867545dd950d1121fdffd86353144563ebf6d
vdhungbg <vdhungbg@gmail.com>
2019-08-04 14:23:12 +0700

    new

--- app/Book.php
+++ app/Book.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     //
+    // protected $fillable = ['book_name', 'isbn_no', 'book_price'];
     protected $fillable = ['book_name', 'isbn_no', 'book_price'];
 }
